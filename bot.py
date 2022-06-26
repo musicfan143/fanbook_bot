547d9ccca4d6bc41778ac0652964067fa72b649e
musicfan143 <108202199+musicfan143@users.noreply.github.com>
2022-06-26 10:29:56 -0400

    Add common updates
    several preparation func

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import requests    
 import websocket
-import rel
 import json
 import os
 
@@ -13,29 +12,36 @@ def send_message(chat_id, message):
   print(r.text)
   return r.text
 
-def on_message(ws, message):
+def processMessage(message):
+  pass
+
+def processUpdates():
+  url = f'https://a1.fanbook.mobi/api/bot/{TOKEN}/getUpdates'
+  data = {"timeout": 60, "limit": 100}
+  headers = {'Content-type': 'application/json'}
+  r = requests.post(url, data=json.dumps(data), headers=headers)
+  # print(r.text)
+  data = json.loads(r.text)
+  messages = data['result']
+  for message in messages:
+    print("-----")
     print(message)
-    # logic to match the message
 
-def on_error(ws, error):
-    print(error)
+def setPrivacy():
+  url = f'https://a1.fanbook.mobi/api/bot/{TOKEN}/setBotPrivacyMode'
+  data = {"owner_id": OWNER_ID, "bot_id": BOT_ID, "enable": True}
+  headers = {'Content-type': 'application/json'}
+  r = requests.post(url, data=json.dumps(data), headers=headers)
+  print(r.text)
 
-def on_close(ws, close_status_code, close_msg):
-    print("### closed ###")
-
-def on_open(ws):
-    print("Opened connection")
+def getBot():
+  url = f'https://a1.fanbook.mobi/api/bot/{TOKEN}/getMe'
+  data = {}
+  headers = {'Content-type': 'application/json'}
+  r = requests.post(url, data=json.dumps(data), headers=headers)
+  print(r.text)
 
 if __name__ == "__main__":
-  # send_message(CHAT_ID, "Test")
-  websocket.enableTrace(True)
-  token = os.getenv('TOKEN')
-  ws = websocket.WebSocketApp(f"wss://a1.fanbook.mobi/api/bot/{token}/getUpdates",
-                              on_open=on_open,
-                              on_message=on_message,
-                              on_error=on_error,
-                              on_close=on_close)
-
-  ws.run_forever(dispatcher=rel)  # Set dispatcher to automatic reconnection
-  rel.signal(2, rel.abort)  # Keyboard Interrupt
-  rel.dispatch()
+ while True:
+  processUpdates()
+  time.sleep(10)
